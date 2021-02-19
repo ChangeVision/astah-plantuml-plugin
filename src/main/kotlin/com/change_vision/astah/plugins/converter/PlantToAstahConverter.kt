4e04cbeaaceb208f61447291d7fcd0924ac5d705
s-hosoai <shintaro.hosoai@gmail.com>
2021-02-19 15:55:52 +0900

    add feature to simple create astah model

--- src/main/kotlin/com/change_vision/astah/plugins/converter/PlantToAstahConverter.kt
+++ src/main/kotlin/com/change_vision/astah/plugins/converter/PlantToAstahConverter.kt
@@ -1,7 +1,13 @@
 package com.change_vision.astah.plugins.converter
 
+import com.change_vision.jude.api.inf.AstahAPI
+import com.change_vision.jude.api.inf.editor.TransactionManager
+import com.change_vision.jude.api.inf.model.IAssociation
+import com.change_vision.jude.api.inf.model.IClass
 import net.sourceforge.plantuml.SourceStringReader
 import net.sourceforge.plantuml.classdiagram.ClassDiagram
+import net.sourceforge.plantuml.cucadiagram.ILeaf
+import net.sourceforge.plantuml.cucadiagram.Link
 
 object PlantToAstahConverter {
     fun convert(text: String) {
@@ -9,15 +15,35 @@ object PlantToAstahConverter {
         reader.blocks.map { it.diagram }.forEach { diagram ->
             when (diagram) {
                 is ClassDiagram -> {
-                    diagram.leafsvalues.forEach { leaf ->
-                        println(leaf.codeGetName)
-                    }
-                    diagram.links.forEach { link ->
-                        println(link.entity1.codeGetName + " " + link.type.toString() + " " + link.entity2.codeGetName)
-                    }
+                    val leafIClassMap =
+                        diagram.leafsvalues.map { leaf ->
+                            Pair(leaf, createClassElement(leaf))
+                        }.toMap()
+                    val linkAssociationMap =
+                        diagram.links.map { link ->
+                            Pair(link, createAssociationElement(link, leafIClassMap))
+                        }.toMap()
                 }
                 else -> System.err.println("Unsupported Diagram : " + diagram.javaClass.name)
             }
         }
+    }
+
+    private val projectAccessor = AstahAPI.getAstahAPI().projectAccessor
+    private val modelEditor = projectAccessor.modelEditorFactory.basicModelEditor
+    private fun createClassElement(leaf: ILeaf): IClass {
+        TransactionManager.beginTransaction()
+        val iClass = modelEditor.createClass(projectAccessor.project, leaf.codeGetName)
+        TransactionManager.endTransaction()
+        return iClass
+    }
+
+    private fun createAssociationElement(link: Link, leafIClassMap: Map<ILeaf, IClass>): IAssociation {
+        TransactionManager.beginTransaction()
+        val e1Class = leafIClassMap[link.entity1]!!
+        val e2Class = leafIClassMap[link.entity2]!!
+        val iAssociation = modelEditor.createAssociation(e1Class, e2Class, link.label.toString(), "", "")
+        TransactionManager.endTransaction()
+        return iAssociation
     }
 }
