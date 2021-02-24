518440a259af15922a78e84d3b638d36fe8e5e40
s-hosoai <shintaro.hosoai@gmail.com>
2021-02-25 00:42:55 +0900

    add mouse wheel font resize

--- src/main/kotlin/com/change_vision/astah/plugins/view/PlantUMLSourceArea.kt
+++ src/main/kotlin/com/change_vision/astah/plugins/view/PlantUMLSourceArea.kt
@@ -1,6 +1,7 @@
 package com.change_vision.astah.plugins.view
 
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea
+import java.awt.Font
 import javax.swing.event.DocumentEvent
 import javax.swing.event.DocumentListener
 
@@ -20,6 +21,21 @@ class PlantUMLSourceArea(private val textChangeAction: (String) -> Any) : RSynta
                 textChangeAction(text)
             }
         })
+        font = Font("Terminal", Font.PLAIN, 12)
+        var fontSize = font.size
+        addMouseWheelListener { e ->
+            if (e.isControlDown) {
+                fontSize += e.wheelRotation
+                fontSize = when {
+                    fontSize > 50 -> 50
+                    fontSize < 8 -> 8
+                    else -> fontSize
+                }
+                font = Font(font.fontName, font.style, fontSize)
+            } else {
+                parent.dispatchEvent(e)
+            }
+        }
     }
 }
 
