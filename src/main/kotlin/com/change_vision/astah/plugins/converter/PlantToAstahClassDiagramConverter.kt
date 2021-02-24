61ffd5c02afbf1ef12f9d480e88ffdb66ebc65e7
s-hosoai <shintaro.hosoai@gmail.com>
2021-02-24 14:06:02 +0900

    implements create presentations without position

--- src/main/kotlin/com/change_vision/astah/plugins/converter/PlantToAstahClassDiagramConverter.kt
+++ src/main/kotlin/com/change_vision/astah/plugins/converter/PlantToAstahClassDiagramConverter.kt
@@ -2,11 +2,15 @@ package com.change_vision.astah.plugins.converter
 
 import com.change_vision.jude.api.inf.AstahAPI
 import com.change_vision.jude.api.inf.editor.TransactionManager
+import com.change_vision.jude.api.inf.exception.BadTransactionException
+import com.change_vision.jude.api.inf.model.IAssociation
 import com.change_vision.jude.api.inf.model.IClass
 import com.change_vision.jude.api.inf.model.IDiagram
 import net.sourceforge.plantuml.SourceStringReader
 import net.sourceforge.plantuml.classdiagram.ClassDiagram
 import net.sourceforge.plantuml.cucadiagram.ILeaf
+import net.sourceforge.plantuml.cucadiagram.Link
+import java.awt.geom.Point2D
 
 
 object PlantToAstahClassDiagramConverter {
@@ -23,13 +27,51 @@ object PlantToAstahClassDiagramConverter {
         // create diagram
         val diagramName = "ClassDiagram_$index"
         val projectAccessor = AstahAPI.getAstahAPI().projectAccessor
-        if (projectAccessor.findElements(IDiagram::class.java, diagramName).isEmpty()) {
-            val diagramEditor = projectAccessor.diagramEditorFactory.classDiagramEditor
-            TransactionManager.beginTransaction()
-            diagramEditor.createClassDiagram(projectAccessor.project, diagramName)
-            TransactionManager.endTransaction()
-        }
+        val diagramEditor = projectAccessor.diagramEditorFactory.classDiagramEditor
+        val foundDiagramList = projectAccessor.findElements(IDiagram::class.java, diagramName)
+        val classDiagram =
+            when {
+                foundDiagramList.isNotEmpty() -> {
+                    foundDiagramList.first() as IDiagram
+                }
+                else -> {
+                    TransactionManager.beginTransaction()
+                    try {
+                        diagramEditor.createClassDiagram(projectAccessor.project, diagramName)
+                            .also { TransactionManager.endTransaction() }
+                    } catch (e: BadTransactionException) {
+                        TransactionManager.abortTransaction()
+                        return
+                    }
+                }
+            }
 
         // convert presentations
+        val api = AstahAPI.getAstahAPI()
+        TransactionManager.beginTransaction()
+        try {
+            val viewElementMap = entityMap.keys.map { entity ->
+                val viewElement =
+                    diagramEditor.createNodePresentation(
+                        entityMap[entity],
+                        Point2D.Float(0f, 0f)
+                    )
+                Pair(entity, viewElement)
+            }.toMap()
+            linkConvertResults
+                .filterIsInstance<Success<Pair<Link, IAssociation>>>()
+                .map { it.convertPair }
+                .forEach { link ->
+                    diagramEditor.createLinkPresentation(
+                        link.second,
+                        viewElementMap[link.first.entity1],
+                        viewElementMap[link.first.entity2]
+                    )
+                }
+            api.viewManager.diagramViewManager.open(classDiagram)
+            TransactionManager.endTransaction()
+        } catch (e: BadTransactionException) {
+            TransactionManager.abortTransaction()
+        }
     }
 }
