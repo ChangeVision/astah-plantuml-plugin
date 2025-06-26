fd56543d1a4c7efcf852c8ce59b07df360d7d183
hinata-ito-77 <hinata.ito@change-vision.com>
2025-06-26 11:33:24 +0900

    アクティビティ図のtoPlantの変更を取り込む
    アクション、制御フロー、デシジョンノード、ジョインノード、フォークノードに対応した。

---
+++ src/main/kotlin/com/change_vision/astah/plugins/converter/toplant/activitydiagram/ActivityDiagramNodeNameFormatter.kt
@@ -0,0 +1,35 @@
+package com.change_vision.astah.plugins.converter.toplant.activitydiagram
+
+import com.change_vision.astah.plugins.converter.toplant.activitydiagram.syntax.LegacyActivityDiagramSyntax
+import com.change_vision.astah.plugins.converter.toplant.node.NodeNameFormatter
+import com.change_vision.jude.api.inf.presentation.INodePresentation
+
+class ActivityDiagramNodeNameFormatter(
+    private val idMap: Map<INodePresentation, String>
+) : NodeNameFormatter {
+
+    private val convertedNodes = mutableSetOf<INodePresentation>()
+
+    override fun format(node: INodePresentation): String {
+        return when {
+            ActivityDiagramNodeType.INITIAL_NODE(node) || ActivityDiagramNodeType.FINAL_NODE(node) ->
+                LegacyActivityDiagramSyntax.initialOrFinalNode()
+
+            ActivityDiagramNodeType.FORK_NODE(node) || ActivityDiagramNodeType.JOIN_NODE(node) -> {
+                val id = idMap[node]
+                LegacyActivityDiagramSyntax.forkJoinNode(node.label, id)
+            }
+
+            else -> {
+                val id = idMap[node]
+                if (id != null) {
+                    if (!convertedNodes.add(node)) {
+                        return LegacyActivityDiagramSyntax.reuseId(id)
+                    }
+                    return LegacyActivityDiagramSyntax.node(label = node.label, alias = id)
+                }
+                return LegacyActivityDiagramSyntax.node(label = node.label)
+            }
+        }
+    }
+}
---
+++ src/main/kotlin/com/change_vision/astah/plugins/converter/toplant/activitydiagram/ActivityDiagramNodeType.kt
@@ -0,0 +1,35 @@
+package com.change_vision.astah.plugins.converter.toplant.activitydiagram
+
+import com.change_vision.astah.plugins.converter.toplant.node.NodeType
+import com.change_vision.jude.api.inf.model.IAction
+import com.change_vision.jude.api.inf.model.IControlNode
+import com.change_vision.jude.api.inf.model.IObjectNode
+import com.change_vision.jude.api.inf.presentation.INodePresentation
+
+enum class ActivityDiagramNodeType(
+    override val labelText: String,
+    private val predicate: (INodePresentation) -> Boolean
+) : NodeType {
+
+    ACTION("Action", { it.model is IAction }),
+    OBJECT_NODE("Object Node", { it.model is IObjectNode }),
+    FORK_NODE("Fork Node", { (it.model as? IControlNode)?.isForkNode == true }),
+    JOIN_NODE("Join Node", { (it.model as? IControlNode)?.isJoinNode == true }),
+    INITIAL_NODE("Initial Node", { (it.model as? IControlNode)?.isInitialNode == true }),
+    FINAL_NODE("Final Node", { (it.model as? IControlNode)?.isFinalNode == true }),
+    DECISION_MERGE_NODE("Decision/Merge Node", { (it.model as? IControlNode)?.isDecisionMergeNode == true }),
+    DECISION_NODE("Decision Node", {
+        DECISION_MERGE_NODE(it)
+                && ActivityConvertUtil.getIncomings(it).size == 1
+                && ActivityConvertUtil.getOutgoings(it).size >= 2
+    }),
+    ;
+
+    override fun invoke(node: INodePresentation): Boolean = predicate(node)
+
+    override val idPrefix: String
+        get() = when (this) {
+            FINAL_NODE -> "X"
+            else -> name.first().toString().lowercase()
+        }
+}
